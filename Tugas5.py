c2538d8afffa37a40dcd810cc63acc50c5a64488
RaviqiIkhsan <166134514+RaviqiIkhsan@users.noreply.github.com>
2024-05-29 22:28:39 +0700

    Add files via upload

---
+++ Tugas5.py
@@ -0,0 +1,22 @@
+#Raviqi Ikhsan Doni
+#1217070069
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread('Kopi.jpg', 0)
+ret, thresh1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+ret, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+ret, thresh3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC)
+ret, thresh4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)
+ret, thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
+titles = ['Original', 'BINARY', 'BINARY_INV', 'TRUNC', 'TOZERO', 'TOZERO_INV']
+images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+plt.rcParams["figure.figsize"] = (12, 12)
+for i in range(6):
+    plt.subplot(3, 2, i + 1)
+    plt.imshow(images[i], cmap='gray')
+    plt.title(titles[i])
+    plt.xticks([]), plt.yticks([])
+plt.tight_layout()  
+plt.show()
